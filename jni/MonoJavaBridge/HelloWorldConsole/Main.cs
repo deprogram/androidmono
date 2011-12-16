0c295bfc41f7297610ceb87a6908ba061d115fa1
Gabe Martin <gabe.martin@logos.com>
2011-12-15 16:49:19 -0800

    Replaced missing project with simple console app and linked it to test.exe.

---
+++ jni/MonoJavaBridge/HelloWorldConsole/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorldConsole
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
