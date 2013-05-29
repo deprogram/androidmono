d7e06d4287cdddcade05c3ad3e879e275505276e
Jeremy Bell <bell.jeremy@gmail.com>
2013-05-29 14:35:01 -0400

    Several updates to the get-mono.sh file: - Now temporarily using my fork of mono, with a patch pre-applied. You   can still specify a local checkout of mono if you wish. - Now building using the master branch of mono (from my patched fork). - Removed unnecessary configure options, like --with-moonlight=no. - Now copying the monodroid assemblies into the install directory.   mono's make install doesn't currently do this for you (and probably   won't for the forseable future, as stated by devs).
    This is a work in progress, and the actual binaries have not yet been
    tested in a real application, and may not work.

--- get-mono.sh
+++ get-mono.sh
@@ -21,98 +21,59 @@ function checkresult
 
 if [ -z "$NDK" ]
 then
-    echo "You must specify the NDK variable to point to the android NDK."
+    echo "FAILED: You must specify the NDK variable to point to the android NDK."
     exit
 fi
 
 if [ -z "$NDK_STANDALONE" ]
 then
-    echo "You must specify the NDK_STANDALONE to point to your standalone environment. Someday this script will make it for you, but not today."
-    echo "for instance:"
-    echo "export SYSROOT=NDK/platforms/android-14/arch-arm"
-    echo "NDK/build/tools/make-standalone-toolchain-sh --platform=android-14 --install-dir=install/dir"
-    echo "export NDK_STANDALONE=install/dir"
+    echo "FAILED:"
+    echo "  You must specify the NDK_STANDALONE to point to your standalone environment. Someday this script will make it for you, but not today."
+    echo "  for instance:"
+    echo "    export SYSROOT=NDK/platforms/android-14/arch-arm"
+    echo "    NDK/build/tools/make-standalone-toolchain-sh --platform=android-14 --install-dir=install/dir"
+    echo "    export NDK_STANDALONE=install/dir"
     exit
 fi
 
 if [ -z "$1" ]
 then
-    echo "No local mono directory supplied. Cloning mono from Github. This may take a while..."
-    echo "You can provide a local mono checkout using: $0 \<path-to-mono-git-repository\>"
-    MONO_GIT=git://github.com/mono/mono.git
-    sleep 4
+    echo "--> No local mono directory supplied. Defaulting to the official Mono github repository. You can provide a local mono checkout using: $0 \<path-to-mono-git-repository\>"
+    MONO_GIT=https://github.com/JeroMiya/mono.git
 else
     MONO_GIT=$1/.git
 fi
 
-echo "=====CHECKING OUT MONO FROM $MONO_GIT====="
-MONO_GIT_CHECKOUT=mono-2-10-9
-echo using branch $MONO_GIT_CHECKOUT
-sleep 4
+echo "--> Using Mono git repository from: $MONO_GIT"
+MONO_GIT_CHECKOUT=master
+echo "--> Using Mono branch $MONO_GIT_CHECKOUT"
 
 ROOT_DIR=$(basename $0)
 pushd $(dirname $0)
 
 echo
-echo "=====CHECKING OUT MONO====="
+echo "--> Checking out Mono"
 
 if [ -d mono ]
 then
-    echo "=====UPDATING EXISTING MONO CHECKOUT====="
-    echo "=====If you get errors, you may want to delete mono and mono_builds to force a clean build====="
+    echo "--> using existing mono checkout"
     pushd mono
     # MONO_SKIP_PATCH=true
     git fetch origin
     git checkout $MONO_GIT_CHECKOUT
-    checkresult 'Error while updating ./mono'
+    checkresult 'FAILED: Could not update ./mono from git'
     popd
+
 else
-    echo "====CLONING MONO FOR THE FIRST TIME INTO /mono===="
+    echo "--> Cloning Mono for the first time into /mono"
     git clone $MONO_GIT mono
     pushd mono
     git checkout $MONO_GIT_CHECKOUT
-    checkresult 'Error while cloning ./mono'
+    checkresult 'FAILED: Could not clone mono into ./mono'
     popd
 fi
 
-echo "====CLEANING UP MONO BUILD DIRECTORIES UNDER mono_build===="
-rm -rf mono_builds
-mkdir -p mono_builds
-
-#echo "=====COMPILING MONO CLASS LIBRARIES FOR MCS BUILD====="
-mkdir -p mono_builds/assemblies
-pushd mono_builds/assemblies
-INSTALL_PREFIX=$(pwd)
-echo INSTALL_PREFIX=$INSTALL_PREFIX
-popd
-
-pushd mono
-echo "===== RUNNING CONFIGURE FOR MONODROID ASSEMBLIES"
-./autogen.sh --with-glib=embedded --prefix=$INSTALL_PREFIX --with-monodroid=yes
-make clean && make && make install
-checkresult "FAILED: MONODROID ASSEMBLIES DID NOT BUILD SUCCESSFULLY"
-popd
-
-
-# you can set $BUILD manually prior to running the script, if you like
-if [ -z "$BUILD" ]
-then
-pushd mono
-BUILD=`./config.guess`
-popd
-# not sure why the above fails on my machine sometimes
-if [ -z "$BUILD" ]
-then
-echo "Couldn't run ./config.guess for some reason. using default BUILD=i686-pc-linux-gnu"
-sleep 6
-BUILD=i686-pc-linux-gnu
-fi
-fi
-
-echo "USING BUILD: $BUILD"
-sleep 6
-
-# Disabling patching for now. May add it back if needed.
+# Disabling patching for now. May add it back if needed, but probably better to use a github fork instead?.
 
 #if [ ! -f MonoActivity/jni/mono.patch ]
 #then
@@ -128,6 +89,47 @@ sleep 6
 #    git apply ../mono.patch
 #    popd
 #fi
+
+if [ -d mono_builds/assemblies ]
+then
+    echo "--> Skipping MonoDroid assembly building. To rebuild, delete the ./mono_builds/assemblies directory."
+else
+    echo "--> Compiling Mono class libraries for MCS build"
+    mkdir -p mono_builds/assemblies
+    pushd mono_builds/assemblies
+    INSTALL_PREFIX=$(pwd)
+    echo INSTALL_PREFIX=$INSTALL_PREFIX
+    popd
+
+    pushd mono
+    echo "--> Running ./mono/configure to build MonoDroid assemblies"
+    ./autogen.sh --with-glib=embedded --prefix=$INSTALL_PREFIX --with-monodroid=yes
+    make clean && make && make install
+    checkresult "FAILED: MonoDroid assemblies did not build successfully"
+    popd
+    echo "--> copying mono/mcs/class/lib/monodroid -> mono_builds/assemblies/lib/mono because for some reason make install doesn't"
+    cp -r mono/mcs/class/lib/monodroid mono_builds/assemblies/lib/mono
+fi
+
+
+# you can set $BUILD manually prior to running the script, if you like
+if [ -z "$BUILD" ]
+then
+pushd mono
+BUILD=`./config.guess`
+popd
+# not sure why the above fails on my machine sometimes
+if [ -z "$BUILD" ]
+then
+echo "--> Couldn't run ./mono/config.guess for some reason. using default BUILD=i686-pc-linux-gnu"
+sleep 6
+BUILD=i686-pc-linux-gnu
+fi
+fi
+
+echo "--> Using BUILD=$BUILD"
+
+
 export SYSROOT=$NDK/platforms/android-14/arch-arm
 export PATH=$NDK_STANDALONE/bin:$PATH
 export CC=arm-linux-androideabi-gcc
@@ -139,32 +141,44 @@ export LD=arm-linux-androideabi-ld
 export RANLIB=arm-linux-androideabi-ranlib
 export STRIP=arm-linux-androideabi-strip
 
-mkdir -p mono_builds/armeabi-v7a
-pushd mono_builds/armeabi-v7a
-INSTALL_PREFIX=$(pwd)
-echo INSTALL_PREFIX=$INSTALL_PREFIX
+if [ -d mono_builds/armeabi ]
+then
+    echo "--> Skipping armeabi runtime building. To rebuild, delete the ./mono_builds/armeabi directory."
+else
+    mkdir -p mono_builds/armeabi
+    pushd mono_builds/armeabi
+    INSTALL_PREFIX=$(pwd)
+    echo INSTALL_PREFIX=$INSTALL_PREFIX
+    popd
+    
+    pushd mono
+    echo "--> Running configure for armv7-a-linux-androideabi"
+    ./autogen.sh --build=$BUILD --host=arm-linux-androideabi --target=arm-linux-androideabi --enable-nls=no --with-mcs-docs=no --with-mcs-build=no --prefix=$INSTALL_PREFIX
+    make clean && make && make install
+    checkresult "FAILED: building armeabi was not successful"
+    popd
+fi
+
+if [ -d mono_builds/armeabi-v7a ]
+then
+    echo "--> Skipping armeabi-v7a runtime building. To rebuild, delete the ./mono_builds/armeabi-v7a directory."
+else
+    mkdir -p mono_builds/armeabi-v7a
+    pushd mono_builds/armeabi-v7a
+    INSTALL_PREFIX=$(pwd)
+    echo INSTALL_PREFIX=$INSTALL_PREFIX
+    popd
+    
+    pushd mono
+    echo "--> Running configure for armeabi-v7a"
+    ./autogen.sh --build=$BUILD --host=armv7-a-linux-androideabi --target=armv7-a-linux-androideabi --enable-nls=no --with-mcs-docs=no --with-mcs-build=no --prefix=$INSTALL_PREFIX
+    make clean && make && make install
+    checkresult "FAILED: building armeabi-v7a was not successful"
+    popd
+fi
+
+
+
 popd
 
-pushd mono
-echo "Running configure for armeabi-v7a"
-./autogen.sh --build=$BUILD --host=armv7-a-linux-androideabi --target=armv7-a-linux-androideabi --enable-nls=no --with-mcs-docs=no --with-mcs-build=no --with-moonlight=no --with-gc=none CFLAGS="-DARM_FPU_VFP=1" CXXFLAGS="-DARM_FPU_VFP" --prefix=$INSTALL_PREFIX
-make clean && make && make install
-checkresult "ERROR: building armeabi-v7a was not successful"
-popd
-
-mkdir -p mono_builds/armeabi
-pushd mono_builds/armeabi
-INSTALL_PREFIX=$(pwd)
-echo INSTALL_PREFIX=$INSTALL_PREFIX
-popd
-
-pushd mono
-echo "Running configure for armv7-a-linux-androideabi"
-./autogen.sh --build=$BUILD --host=arm-linux-androideabi --target=arm-linux-androideabi --enable-nls=no --with-mcs-docs=no --with-mcs-build=no --with-moonlight=no --with-gc=none CFLAGS="-DARM_FPU_NONE=1" CXXFLAGS="-DARM_FPU_NONE=1" --prefix=$INSTALL_PREFIX
-make clean && make && make install
-checkresult "ERROR: building armeabi was not successful"
-popd
-
-popd
-
-echo "===== Done! ====="
+echo "--> Done!"
